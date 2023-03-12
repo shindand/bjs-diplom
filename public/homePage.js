371ddc177faab17367aee710bb4a49c1c67ffd34
warvic <warvic@yandex.ru>
2023-03-12 11:18:47 +0300

    кроме обновления курса

--- public/homePage.js
+++ public/homePage.js
@@ -1,7 +1,7 @@
 
 const userFormOut = new LogoutButton();
-userFormOut.action = data => ApiConnector.login({login: data.login, password: data.password}, response => {
-    if (response) {
+userFormOut.action = data => ApiConnector.logout({data}, response => {
+    if (response.success) {
        location.reload();
      }
  });
@@ -11,28 +11,61 @@ userFormOut.action = data => ApiConnector.login({login: data.login, password: da
 ApiConnector.current(response => ProfileWidget.showProfile(response.data));
 
 
+ApiConnector.getStocks(response => userExchangeRate.fillTable(response.data));
 
-function exchangeRate() {
-    userExchangeRate.clearTable();
-    ApiConnector.getStocks(response => userExchangeRate.fillTable(response.data));
-    
+function exchangeRate() {  
+    ApiConnector.getStocks(response => {if (response.success) {
+            userExchangeRate.clearTable();
+            userExchangeRate.fillTable(response.data);
+          }
+     });
 }
 
 
 let userExchangeRate = new RatesBoard();
-setInterval(() => exchangeRate(), 5000);
-
-
-
-let userMoneyManager = new MoneyManager();
+setInterval(() => exchangeRate(), 2000);
 
 
 let userFavorites = new FavoritesWidget();
-ApiConnector.getFavorites(response => userFavorites.fillTable(response.data));
+ApiConnector.getFavorites(response => (userFavorites.clearTable(), userFavorites.fillTable(response.data), userMoneyManager.updateUsersList(response.data)));
 
 
-ApiConnector.getFavorites(response => userFavorites.updateUsersList(response.data));
+userFavorites.addUserCallback = data => ApiConnector.addUserToFavorites(data, response => (data, (userFavorites.clearTable(), userFavorites.fillTable(response.data))));
 
 
+userFavorites.removeUserCallback = data => ApiConnector.removeUserFromFavorites(data, response => (userFavorites.clearTable(), userFavorites.fillTable(response.data)));
 
 
+let userMoneyManager = new MoneyManager();
+userMoneyManager.addMoneyCallback = data => ApiConnector.addMoney(data, response => {
+    if (response.success) {
+        ProfileWidget.showProfile(response.data);
+        userMoneyManager.setMessage(response.success, "Вам денежка пришла !!!");
+    }    
+    else {
+        userMoneyManager.setMessage(response.success, "Сожалею, но что-то пошло не так, как хотелось");
+    }
+    
+});
+
+userMoneyManager.conversionMoneyCallback = data => ApiConnector.convertMoney(data, response => {
+    if (response.success) {
+        ProfileWidget.showProfile(response.data);
+        userMoneyManager.setMessage(response.success, "В мое время, за махинации с валютой сажали...");
+    }    
+    else {
+        userMoneyManager.setMessage(response.success, "Сожалею, но что-то пошло не так, как хотелось");
+    }
+    
+});
+
+userMoneyManager.sendMoneyCallback = data => ApiConnector.transferMoney(data, response => {
+    if (response.success) {
+        ProfileWidget.showProfile(response.data);
+        userMoneyManager.setMessage(response.success, "Ваши денежки тю-тю. Сами того пожелали");
+    }    
+    else {
+        userMoneyManager.setMessage(response.success, "Сожалею, но что-то пошло не так, как хотелось");
+    }
+    
+});
