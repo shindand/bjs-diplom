3d32eead5f4cf3b523e4749e58ab0948a801e9fe
warvic <warvic@yandex.ru>
2023-03-16 10:01:22 +0300

    не работает обновление валюты

--- public/homePage.js
+++ public/homePage.js
@@ -1,17 +1,16 @@
 
 const userFormOut = new LogoutButton();
-userFormOut.action = data => ApiConnector.logout({data}, response => {
+userFormOut.action = data => ApiConnector.logout(response => {
     if (response.success) {
        location.reload();
      }
  });
 
 
-
 ApiConnector.current(response => ProfileWidget.showProfile(response.data));
 
 
-ApiConnector.getStocks(response => userExchangeRate.fillTable(response.data));
+let userExchangeRate = new RatesBoard();
 
 function exchangeRate() {  
     ApiConnector.getStocks(response => {if (response.success) {
@@ -21,31 +20,44 @@ function exchangeRate() {
      });
 }
 
-
-let userExchangeRate = new RatesBoard();
+exchangeRate();
 setInterval(() => exchangeRate(), 2000);
 
 
 let userFavorites = new FavoritesWidget();
-ApiConnector.getFavorites(response => (userFavorites.clearTable(), userFavorites.fillTable(response.data), userMoneyManager.updateUsersList(response.data)));
 
+ApiConnector.getFavorites(response => {
+    if (response.success) {
+        userFavorites.clearTable();
+        userFavorites.fillTable(response.data);
+        userMoneyManager.updateUsersList(response.data);
+    }
+});
 
-userFavorites.addUserCallback = data => ApiConnector.addUserToFavorites(data, response => (data, (userFavorites.clearTable(), userFavorites.fillTable(response.data))));
+userFavorites.addUserCallback = data => ApiConnector.addUserToFavorites(data, response => {
+    if (response.success) {
+        userFavorites.clearTable();
+        userFavorites.fillTable(response.data);
+     }
+ });
 
-
-userFavorites.removeUserCallback = data => ApiConnector.removeUserFromFavorites(data, response => (userFavorites.clearTable(), userFavorites.fillTable(response.data)));
+userFavorites.removeUserCallback = data => ApiConnector.removeUserFromFavorites(data, response => {
+    if (response.success) {
+        userFavorites.clearTable();
+        userFavorites.fillTable(response.data);
+    }
+});
 
 
 let userMoneyManager = new MoneyManager();
 userMoneyManager.addMoneyCallback = data => ApiConnector.addMoney(data, response => {
     if (response.success) {
         ProfileWidget.showProfile(response.data);
-        userMoneyManager.setMessage(response.success, "Вам денежка пришла !!!");
+        userMoneyManager.setMessage(response.success, "Вам денежка пришла");
     }    
     else {
-        userMoneyManager.setMessage(response.success, "Сожалею, но что-то пошло не так, как хотелось");
+        userMoneyManager.setMessage(response.success, response.error);
     }
-    
 });
 
 userMoneyManager.conversionMoneyCallback = data => ApiConnector.convertMoney(data, response => {
@@ -54,9 +66,8 @@ userMoneyManager.conversionMoneyCallback = data => ApiConnector.convertMoney(dat
         userMoneyManager.setMessage(response.success, "В мое время, за махинации с валютой сажали...");
     }    
     else {
-        userMoneyManager.setMessage(response.success, "Сожалею, но что-то пошло не так, как хотелось");
+        userMoneyManager.setMessage(response.success, response.error);
     }
-    
 });
 
 userMoneyManager.sendMoneyCallback = data => ApiConnector.transferMoney(data, response => {
@@ -65,7 +76,6 @@ userMoneyManager.sendMoneyCallback = data => ApiConnector.transferMoney(data, re
         userMoneyManager.setMessage(response.success, "Ваши денежки тю-тю. Сами того пожелали");
     }    
     else {
-        userMoneyManager.setMessage(response.success, "Сожалею, но что-то пошло не так, как хотелось");
+        userMoneyManager.setMessage(response.success, response.error);
     }
-    
 });
