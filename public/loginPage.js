d0d6d531d1bb26599632b4d4292563e3505e8659
warvic <warvic@yandex.ru>
2023-03-16 09:59:56 +0300

    теперь-то все, надеюсь

--- public/loginPage.js
+++ public/loginPage.js
@@ -1,21 +1,21 @@
 "use strict"     
 
 const userFormLogin = new UserForm();
- userFormLogin.loginFormCallback = data => ApiConnector.login({data}, response => {
+ userFormLogin.loginFormCallback = data => ApiConnector.login(data, response => {
    if (response.success) {
       location.reload();
     }
     else {
-       userFormLogin.setLoginErrorMessage("Ошибка авторизации. Неверный пароль/логин");
+       userFormLogin.setLoginErrorMessage(response.success, response.error);
     }
 });
 
-  userFormLogin.registerFormCallback = data => ApiConnector.login({data}, response => {
+  userFormLogin.registerFormCallback = data => ApiConnector.login(data, response => {
     if (response.success) {
        location.reload();
      }
      else {
-       userFormLogin.setLoginErrorMessage("Ошибка регистрации. Попробуйте еще раз");
+       userFormLogin.setLoginErrorMessage(response.success, response.error);
      }
  });
  
